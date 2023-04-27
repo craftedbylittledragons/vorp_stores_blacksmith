002b8da9b34a0c81f9fef4802493ad068af21b28
Crafted By Little Dragons <elsandkls@kidshideaway.net>
2023-04-27 02:16:15 -0400

    updated readme and version data
    updated readme and version data

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,9 @@
 fx_version 'adamant'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-
 game 'rdr3'
-author 'VORP @outsider31000'
+description 'Vorp Stores Blacksmith, AI store, Buy and Sell, Locations, Items'
+author 'VORP @outsider31000 MOD by Crafted by Little Dragons'
 lua54 'yes'
-
 
 client_scripts { 'client/client.lua' }
 
@@ -18,14 +17,16 @@ shared_scripts {
 
 dependencies {
     'menuapi',
-    'vorp_inputs' -- download from the vorp github
-}
+    'vorp_inputs', -- download from the vorp github
+    'vorp_core', -- download from the vorp github
+    'vorp_inventory', -- download from the vorp github
+} 
 
 
 --dont
 --touch
 
-version '1.61'
+version '1.6.1'
 vorp_checker 'yes'
 vorp_name '^4Resource version Check^3'
 vorp_github 'https://github.com/craftedbylittledragons/vorp_stores_blacksmith'
